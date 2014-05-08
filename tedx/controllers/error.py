962309ce6a1cb83195027e5d6cef2624a1879282
carlos <carlos.val@bifi.es>
2014-05-08 13:56:57 +0200

    Nueva forma de mostrar los errores

--- tedx/controllers/error.py
+++ tedx/controllers/error.py
@@ -1,5 +1,7 @@
 import cgi
 
+import logging
+
 from paste.urlparser import PkgResourcesParser
 from pylons import request
 from pylons.controllers.util import forward
@@ -7,6 +9,8 @@ from pylons.middleware import error_document_template
 from webhelpers.html.builder import literal
 
 from tedx.lib.base import BaseController, render
+
+log = logging.getLogger(__name__)
 
 class ErrorController(BaseController):
 
@@ -21,17 +25,25 @@ class ErrorController(BaseController):
     """
 
     def document(self):
-
-        return render('/404.mako')
+        function ='document'
+        log.debug(function)
 
         """Render the error document"""
         resp = request.environ.get('pylons.original_response')
-        content = literal(resp.body) or cgi.escape(request.GET.get('message', ''))
-        page = error_document_template % \
-            dict(prefix=request.environ.get('SCRIPT_NAME', ''),
-                 code=cgi.escape(request.GET.get('code', str(resp.status_int))),
-                 message=content)
-        return page
+        log.debug('%s resp:%s' % (function, str(resp.status_int)))
+
+        try:
+            return render('/error/%s.mako'%resp.status_int)
+        except:
+            return render('/error/500.mako')
+
+            content = literal(resp.body) or cgi.escape(request.GET.get('message', ''))
+            page = error_document_template % \
+                dict(prefix=request.environ.get('SCRIPT_NAME', ''),
+                    code=cgi.escape(request.GET.get('code', str(resp.status_int))),
+                    message=content)
+            return page
+
 
     def img(self, id):
         """Serve Pylons' stock images"""
