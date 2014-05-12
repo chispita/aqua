62a5bf09e5258112df196207f02620bc6d502188
carlos <carlos.val@bifi.es>
2014-05-12 18:18:37 +0200

    Add some new librarys to helpers

--- tedx/lib/helpers.py
+++ tedx/lib/helpers.py
@@ -13,6 +13,9 @@ from webhelpers.html.tags import link_to
 from webhelpers.html.tags import *
 from webhelpers.html.tags import file
 
+from pylons import session
+from sqlalchemy.orm.util import object_mapper
+
 
 def toJSON(obj):
     return simplejson.encoder.JSONEncoder(use_decimal=True, sort_keys=True, encoding="utf-8").encode(obj)
@@ -51,3 +54,48 @@ def generate_vcard(user, path, size):
 
     qr = enc.encode(vcard_text.encode("utf8"), {'width': w, 'mode': 2})
     qr.save(path)
+
+
+def check_flash():
+    """If the session data isn't of the particular format python has trouble.
+    So we check that it is a dict."""
+    if session.has_key('flash'):
+        if type(session['flash']) != dict:
+            del session['flash']
+            session.save()
+
+def get_flashes():
+    check_flash()
+    if not session.has_key('flash'):
+        return None
+    messages = session['flash']
+    # it is save to delete now
+    del(session['flash'])
+    session.save()
+    return messages
+
+
+def flash(msg, category="information"):
+    check_flash()
+    if not session.has_key('flash'):
+        session['flash'] = {}
+    if not session['flash'].has_key(category):
+        session['flash'][category] = []
+    session['flash'][category].append(msg)
+    session.save()
+
+def object_to_defaults(object, prefix):
+    defaults = {}
+
+    for key in object_mapper(object).columns.keys():
+        value = getattr(object, key)
+        if type(value) == list:
+            for code in value:
+                defaults['.'.join((prefix, key, code))] = 1
+                defaults['.'.join((prefix, key))] = ','.join(value)
+        elif value == True:
+            defaults['.'.join((prefix, key))] = 1
+        else:
+            defaults['.'.join((prefix, key))] = value
+
+    return defaults
