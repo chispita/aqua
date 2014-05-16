0423aac25070f1b668dddbb10eb7baf355b495d6
carlos <carlos.val@bifi.es>
2014-05-16 10:23:58 +0200

    Generic information

---
+++ tedx/config/lca_info.py
@@ -0,0 +1,8 @@
+
+lca_info = {
+    'contact_email' : 'aqua@ibercivis.es',
+    'webmaster_email' : 'webmaster@ibercivis.es',
+    'bcc_email' : 'carlos.val@ibercivis.es',
+    'event_name' : 'Aqua Server'
+}
+
